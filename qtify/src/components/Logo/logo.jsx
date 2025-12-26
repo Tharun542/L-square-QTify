27200edfa66d4179e788875dd6d4ab088fa79b0e
tharun <godasutharun143@gmail.com>
2025-12-26 17:24:32 +0530

    Fix navbar logo alignment

--- qtify/src/components/Logo/logo.jsx
+++ qtify/src/components/Logo/logo.jsx
@@ -3,7 +3,7 @@ import logo from "../../assets/logo.SVG";
 import "./logo.css";
 
 const Logo =()=>{
-    return <img src={logo} alt="Qtify Logo" />;
+    return <img src={logo} alt="Qtify Logo" className="logo" />;
 }
 
 export default Logo;

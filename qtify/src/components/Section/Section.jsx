796b91a68b5beb625aa9c2262197df65f78f00fd
tharun <godasutharun143@gmail.com>
2025-12-27 13:23:59 +0530

    error fixed card and section

--- qtify/src/components/Section/Section.jsx
+++ qtify/src/components/Section/Section.jsx
@@ -4,20 +4,20 @@ import FetchTopAlbums from "../Api/Api";
 import Card from "../Card/Card"
 
  const Section=()=>{
-  const [albums, setAlbums] = useState([]);
+  const [topAlbums, setTopAlbums] = useState([]);
 
   useEffect(()=>{
-    FetchTopAlbums().then((data)=>{setAlbums(data)})
+    FetchTopAlbums().then((data)=>{setTopAlbums(data)})
   },[])
 
   return(
     <div className="section">
         <div className="section1">
-            <h1 className="top">top Albums</h1>
-            <button className="btn">collapse</button>
+            <h1 className="top">Top Albums</h1>
+            <button className="btn">Collapse</button>
         </div>
         <div className="albums">
-          {albums.map((album)=>(
+          {topAlbums.map((album)=>(
             <Card key={album.id} title={album.title} image={album.image} follows={album.follows}/>
         ))}
         </div>
